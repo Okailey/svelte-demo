390e5bab2bf911f62c73b25efedf21b94b23661a
Okailey <quaye22d@mtholyoke.edu>
2025-06-08 23:26:27 -0400

    played around with +page.svelte

---
+++ src/routes/about/+page.svelte
@@ -0,0 +1,16 @@
+<svelte:head>
+	<title>About</title>
+	<meta name="description" content="About this app" />
+</svelte:head>
+
+<div class="text-column">
+	<h1>Trying to deploy this app</h1>
+
+	<p>
+		This is a <a href="https://svelte.dev/docs/kit">SvelteKit</a> app. You can
+		make your own by typing the following into your command line and following
+		the prompts:
+	</p>
+
+	<pre>npx sv create</pre>
+</div>
